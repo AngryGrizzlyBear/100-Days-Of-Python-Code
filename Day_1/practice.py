6766a972dd9467110352246284ea5ef78e9d3237
Evan Longwood <24954397+AngryGrizzlyBear@users.noreply.github.com>
2026-06-12 17:01:54 -0700

    Day One project complete

---
+++ Day_1/practice.py
@@ -0,0 +1,17 @@
+
+# print("tupac\nisn't dead.\nHis name is Greg.")
+# print("Hello" + " " +  "Tupac")
+
+# print("Hello " + input("What is your name? ")+ "!")
+
+# name = input("What is your name?")
+# print(name);
+
+# name = input("What is your name?")
+# print("Hello " + name + "!")
+# print("Your name is", len(name), "numbers long!")
+
+username = input("What is your name?")
+length = len(username)
+print(length)
+
---
+++ day_one_project.py
@@ -0,0 +1,6 @@
+home_city = input("Where are you from? ")
+name_of_first_pet = input("What was the name of your first pet? ")
+
+print( "Your band name could be", home_city + " " + name_of_first_pet)
+
+
