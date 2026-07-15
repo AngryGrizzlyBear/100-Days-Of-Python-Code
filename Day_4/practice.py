df9f0cf746d0e840d26bf0609c368a063b179e47
Evan Longwood <24954397+AngryGrizzlyBear@users.noreply.github.com>
2026-07-15 08:25:25 -0700

    Day_4

---
+++ Day_4/practice.py
@@ -0,0 +1,15 @@
+import random
+
+# randomInterger = random.randint(0, 1)
+# print(randomInterger)
+
+
+# if randomInterger == 0:
+#     print("Heads")
+# else:
+#     print("Tails")
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+
+print(random.choice(friends))
